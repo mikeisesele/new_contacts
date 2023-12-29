ec98c45c9fe62e8d9aa14282bbd5d49addc62f8e
mike <michaelisesele@gmail.com>
2023-12-29 17:34:02 +0100

    set up application

--- app/src/main/java/com/michael/template/feature/entrypoint/Application.kt
+++ app/src/main/java/com/michael/template/feature/entrypoint/Application.kt
@@ -1,7 +1,41 @@
 package com.michael.template.feature.entrypoint
 
 import android.app.Application
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import com.michael.template.core.data.SharedPref
+import com.michael.template.util.Constants
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltAndroidApp
-class Application : Application()
+class Application : Application(), LifecycleObserver {
+
+    @Inject
+    lateinit var sharedPref: SharedPref
+
+    override fun onCreate() {
+        super.onCreate()
+//        Timber.plant(Timber.DebugTree())
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onAppBackgrounded() {
+        // App in background
+        CoroutineScope(Dispatchers.Main).launch {
+            sharedPref.saveToSharedPref(Constants.IS_APP_ACTIVE, false)
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onAppForegrounded() {
+        // App in foreground
+        CoroutineScope(Dispatchers.Main).launch {
+            sharedPref.saveToSharedPref(Constants.IS_APP_ACTIVE, true)
+        }
+    }
+}
