851927569696165f2c06fbcdea68a23d04984811
mike <michaelisesele@gmail.com>
2024-01-10 18:07:21 +0100

    fix time ago

--- app/src/main/java/com/michael/template/core/base/util/Extensions.kt
+++ app/src/main/java/com/michael/template/core/base/util/Extensions.kt
@@ -1,5 +1,13 @@
 package com.michael.template.core.base.util
 
+import androidx.annotation.ColorInt
+import androidx.core.graphics.ColorUtils
+import com.michael.template.util.Constants.FLOAT_FIVE
+import com.michael.template.util.Constants.FLOAT_FOUR
+import com.michael.template.util.Constants.THIRTY_SEVEN
+import com.michael.template.util.Constants.THREE_SIXTY
+import kotlin.math.absoluteValue
+
 fun <T, K> Iterable<T>.toSetBy(selector: (T) -> K): Set<T> {
     val set = LinkedHashSet<T>()
     val keys = HashSet<K>()
@@ -10,4 +18,15 @@ fun <T, K> Iterable<T>.toSetBy(selector: (T) -> K): Set<T> {
         }
     }
     return set
+}
+
+fun String.normalize(): String {
+    // Remove non-digit characters and leading country code
+    return replace(Regex("[^\\d]"), "").removePrefix("+234").removePrefix("0")
+}
+
+@ColorInt
+fun String.toHslColor(saturation: Float = FLOAT_FIVE, lightness: Float = FLOAT_FOUR): Int {
+    val hue = fold(0) { acc, char -> char.code + acc * THIRTY_SEVEN } % THREE_SIXTY
+    return ColorUtils.HSLToColor(floatArrayOf(hue.absoluteValue.toFloat(), saturation, lightness))
 }
--- app/src/main/java/com/michael/template/util/TimeUtil.kt
+++ app/src/main/java/com/michael/template/util/TimeUtil.kt
@@ -1,10 +1,14 @@
 package com.michael.template.util
 
+import com.michael.template.util.Constants.ONE
+import com.michael.template.util.Constants.ONE_THOUSAND
+import com.michael.template.util.Constants.SIXTY
+import com.michael.template.util.Constants.THIRTY
+import com.michael.template.util.Constants.THREE_SIX_FIVE
+import com.michael.template.util.Constants.TWELVE
+import com.michael.template.util.Constants.TWENTY_FOUR
 import java.time.ZoneId
 import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
-import java.time.temporal.ChronoUnit
-import java.util.Locale
 
 fun ZonedDateTime?.isWithinDaysFromToday(days: Long): Boolean = this?.let {
     val currentTime = ZonedDateTime.now(ZoneId.systemDefault())
@@ -13,26 +17,53 @@ fun ZonedDateTime?.isWithinDaysFromToday(days: Long): Boolean = this?.let {
 
 fun ZonedDateTime.isBeforeOrEqual(other: ZonedDateTime): Boolean = isBefore(other) || isEqual(other)
 
-@Suppress("MagicNumber")
+@Suppress("ComplexMethod")
 fun ZonedDateTime.toReadable(): String {
-    val currentTime = ZonedDateTime.now(ZoneId.systemDefault())
-    val hoursAgo = ChronoUnit.HOURS.between(this, currentTime)
+    val diff = ZonedDateTime.now().toInstant().toEpochMilli() - toInstant().toEpochMilli()
+
+    val oneSec = ONE_THOUSAND
+    val oneMin: Long = SIXTY * oneSec
+    val oneHour: Long = SIXTY * oneMin
+    val oneDay: Long = TWENTY_FOUR * oneHour
+    val oneMonth: Long = THIRTY * oneDay
+    val oneYear: Long = THREE_SIX_FIVE * oneDay
+
+    val diffMin: Long = diff / oneMin
+    val diffHours: Long = diff / oneHour
+    val diffDays: Long = diff / oneDay
+    val diffMonths: Long = diff / oneMonth
+    val diffYears: Long = diff / oneYear
 
     return when {
-        hoursAgo in 0..23 -> "Added today"
-        hoursAgo in 24..47 -> "Added yesterday"
-        hoursAgo in 48..168 -> "Added earlier this week"
-        hoursAgo in 169..335 -> "Added last week"
-        hoursAgo in 336..480 -> "Added two weeks ago"
-        hoursAgo in 481..623 -> "Added three weeks ago"
-        hoursAgo >= 624 -> {
-            val weeksAgo = hoursAgo / 168
-            "Added $weeksAgo weeks ago"
+        diffYears > 0 -> {
+            "$diffYears years ago"
         }
+        diffMonths > 0 && diffYears < ONE -> {
+            val months = (diffMonths - diffYears / TWELVE)
+            val monthsDisplay = if (months == ONE) "month" else "months"
+            "$monthsDisplay months ago"
+        }
+        diffDays > 0 && diffMonths < ONE -> {
+            val day = (diffDays - diffMonths / THIRTY)
+            val daysDisplay = if (day > ONE) "days" else "day"
+            "$day $daysDisplay ago"
+        }
+        diffHours > 0 && diffDays < ONE -> {
+            val hours = (diffHours - diffDays * TWENTY_FOUR)
+            val hoursDisplay = if (hours > ONE) "hours" else "hour"
+            "$hours $hoursDisplay ago"
+        }
+        diffMin > 0 && diffHours < ONE -> {
+            val min = diffMin % SIXTY
+            val minDisplay = if (min == ONE) "minute" else "minutes"
+            return "$min $minDisplay ago"
+        }
+        diffMin < ONE -> {
+            "just now"
+        }
+
         else -> {
-            // If the date is not recent, format it as "Added on" with a custom date format
-            val formattedDate = this.format(DateTimeFormatter.ofPattern("MMM d, yyyy", Locale.getDefault()))
-            "Added on $formattedDate"
+            "Added some time ago"
         }
     }
 }
